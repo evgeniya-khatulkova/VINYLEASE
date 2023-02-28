c6496a789b617dabc4687768071e3e6b8abdf775
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-02-28 11:21:43 +0100

    add review model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_163408) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_095903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_163408) do
     t.string "artist"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_records_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
