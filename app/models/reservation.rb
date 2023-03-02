fc4c6c8e8f34f2aae5698a157cfa2d7944985df0
Iris D <iris.dijkstra@hotmail.com>
2023-03-02 12:19:36 +0100

    destroy reviews only via record - reservations done (removed comment)

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :record
-  # Only destroy when deleted via records
-  has_many :reviews #, dependent: :destroy
-  
+  has_many :reviews
 end
