1221a741d43ac19ace51a8fc1dc96a7788d0d273
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-02 11:23:07 +0100

    align the reservations list

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :records, only: [:new, :create, :show, :destroy, :update, :edit] do
+  resources :records, only: %i[new create show destroy update save edit] do
     resources :reservations, only: [:index, :new, :create, :show]
   end
 
   resources :reservations do
     resources :reviews, only: %i[index new create show destroy]
   end
+
   devise_for :users
 
   # only: %i[index new create]
