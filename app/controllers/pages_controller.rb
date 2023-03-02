22ed35cf21384db6a09e5a2ba2f3b785eb45851a
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-02 17:50:05 +0100

    make work a searching form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,23 @@
 class PagesController < ApplicationController
   def home
     @records = Record.all
-    if params[:genre].present?
-      @records = Record.where(genre: [params[:genre]])
+      if params[:genre].present?
+        @records = Record.where(genre: [params[:genre]])
+      end
+    if params[:query].present?
+      sql_query = "artist ILIKE :query OR title ILIKE :query"
+      @records = Record.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @records = Record.all
     end
   end
 
   def rock
   end
 
-private
+  private
 
-def record_params
-  params.require(:record).permit(%i[title genre price location artist])
-end
+  def record_params
+    params.require(:record).permit(%i[title genre price location artist])
+  end
 end
