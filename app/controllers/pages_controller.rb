5c79aeec4b1877cf7d6a1eaa4852c83f24d2b18b
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-02 19:55:49 +0100

    add navbar layout and log out sign in fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,13 @@ class PagesController < ApplicationController
   def home
     @records = Record.all
       if params[:genre].present?
-        @records = Record.where(genre: [params[:genre]])
+        @records = Record.where(genre:[params[:genre]])
       elsif params[:query].present?
-      sql_query = "artist ILIKE :query OR title ILIKE :query"
-      @records = Record.where(sql_query, query: "%#{params[:query]}%")
-    else
-      @records = Record.all
-    end
+        sql_query = "artist ILIKE :query OR title ILIKE :query"
+        @records = Record.where(sql_query, query: "%#{params[:query]}%")
+      else
+        @records = Record.all
+      end
   end
 
   def rock
